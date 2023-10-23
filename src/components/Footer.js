8dc19f93a2678e86224b46976ee1cb8a7682b2d5
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-10-23 04:19:48 +0200

    add a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 text-base-content">
+    <aside>
+        <p>Copyright © 2023 - All right reserved by PathPioneer</p>
+    </aside>
+</footer>
+  );
+};
+
+export default Footer;
