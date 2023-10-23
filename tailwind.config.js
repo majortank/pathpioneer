a36496a4ed1596259631adfae2ab930689c3f1a1
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-10-23 04:18:21 +0200

    Create pp custom color theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,29 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  daisyui: {
+    themes: [
+      {
+        pp: {
+            "primary": "#6366f1",
+                    
+            "secondary": "#d946ef",
+                    
+            "accent": "#1dcdbc",
+                    
+            "neutral": "#2b3440",
+                    
+            "base-100": "#ffffff",
+                    
+            "info": "#3abff8",
+                    
+            "success": "#36d399",
+                    
+            "warning": "#fbbd23",
+                    
+            "error": "#f87272",
+                    },
+                  },
+                ],
+              },
   plugins: [require('daisyui')],
 };
