e54747f52803b1ffa70c077a446409e6a7c8cd91
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-10-23 04:20:14 +0200

    Create a reward punishment component.

---
+++ src/components/RewardPunishment.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+
+const RewardPunishment = ({ isCorrect }) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setVisible(false);
+    }, 3000); // Hide the emoji after 3 seconds (3000 milliseconds)
+
+    return () => clearTimeout(timeout); // Clean up the timeout on component unmount or re-render
+  }, []); // Empty dependency array ensures the effect runs only once after the initial render
+
+  return visible ? (
+    <>
+      {isCorrect ? (
+        <>
+          <div className="indicator-item indicator-middle indicator-center text-8xl">
+            <span className=' animate-ping flex flex-col-reverse text-success'>👍 Nooice!</span>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="indicator-item indicator-middle indicator-center text-8xl">
+            <span className=' animate-ping flex flex-col-reverse text-error'>👎 Oops!</span>
+          </div>
+        </>
+      )}
+    </>
+  ) : null;
+};
+
+export default RewardPunishment;
