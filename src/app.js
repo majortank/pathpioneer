a48c2db684e929524715b0f16b7569ffbe8d607c
Thabo Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-10-27 12:54:55 +0200

    Update Enpoint link for deployment.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import SkillsMatrix from "./components/SkillsMatrix";
 import Footer from "./components/Footer";
 import RewardPunishment from "./components/RewardPunishment";
 
-const API_BASE_URL = "http://10.82.1.228:8000/question/all/";
+const API_BASE_URL = "http://pioneerapi.tangikuu.tech/question/all/";
 // const API_BASE_URL = "http://localhost:8000/question/all/";
 // import logo from "images/logo.png";
 
