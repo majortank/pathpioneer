60bb325a9541662a632f890f1ab1a16575bfb9a0
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-11-13 05:37:40 +0200

    Switch from BrowserRouter to HashRouter.

--- src/app.js
+++ src/app.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import './app.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar'; // Import your Navbar component
-import HomePage from './HomePage'; // Import your HomePage component
-import LoginPage from './LoginPage'; // Import your LoginPage component
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
 import SignUpPage from './SignUpPage';
-// import ProfilePage from './ProfilePage'; // Import your ProfilePage component
-// import SettingsPage from './SettingsPage'; // Import your SettingsPage component
 
 const App = () => {
   return (
     <Router>
-      <Navbar /> {/* Display the Navbar on every page */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<SignUpPage />} />
-        {/* <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/settings" element={<SettingsPage />} /> */}
-        {/* Other routes */}
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
