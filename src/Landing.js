a34ab123949df5ac2033bae870d5348c096adece
Thabo Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-11-28 12:08:11 +0200

    Add a Get started buttion and make better gradient

--- src/Landing.js
+++ src/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './app.css';
+import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <div className=''>
@@ -13,8 +14,14 @@ const Landing = () => {
                 </a>
                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-primary md:text-5xl lg:text-6xl dark:text-white">Unlock your potential today!</h1>
                 <p className="mb-8 text-lg font-normal text-gray-700 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">PathPioneer tackles interview nerves, career uncertainties, and more. We empower IT interns to ace interviews, navigate networking, and master time management, guiding them to confidently secure their ideal career in the dynamic IT landscape.</p>
+                <Link to={'/pioneer'} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-secondary hover:bg-primary focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                    Get started
+                    <svg className="w-3.5 h-3.5 ml-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </Link>
             </div>
-            <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+            <div className="bg-gradient-to-b from-indigo-200 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
         </section>
 
         {/*  */}
@@ -29,7 +36,7 @@ const Landing = () => {
                 </a>
                 <h1 className="text-primary dark:text-white text-3xl md:text-5xl font-extrabold mb-2">How to Use Our API for Custom Questions?</h1>
                 <p className="text-lg font-normal text-gray-600 dark:text-gray-400 mb-6">Explore the capabilities of our API, empowering you to craft custom questions and leverage its potential for various purposes within the IT landscape.</p>
-                <a href="https://www.pioneerapi.tangikuu.tech/" className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-secondary hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                <a href="https://www.pioneerapi.tangikuu.tech/" className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-secondary hover:bg-primary focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                     Read more
                     <svg className="w-3.5 h-3.5 ml-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
