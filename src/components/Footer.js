df53a08261d6eddeff75177da6ddb279f2d1381b
Thabo Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2023-11-28 12:08:46 +0200

    Give footer a better bg color.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 // Footer.js
 import React from 'react';
+import '../app.css';
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 text-base-content">
+    <footer className="footer footer-center p-4 text-base-content bg-indigo-100">
     <aside>
         <p>Copyright © 2023 - All right reserved by PathPioneer</p>
     </aside>
