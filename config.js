65882e1d37acb9e3df2f13ea2e716e9a6369fdb7
Thabo Tankiso Thebe <41399491+majortank@users.noreply.github.com>
2024-01-19 11:23:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCqZbXGz1R11xNNEWhP-iVit9aTO2hSzNY',
-  authDomain: 'pathpioneer-12684.firebaseapp.com',
-  projectId: 'pathpioneer-12684',
-  storageBucket: 'pathpioneer-12684.appspot.com',
-  messagingSenderId: '621281019618',
-  appId: '1:621281019618:web:0e085369f0ef0f3fad80bb',
-  measurementId: 'G-WXRTS1Q1CB',
+  apiKey: "AIzaSyAvs3e4QyTJtIHMUjB88mysT-zqo2QTXko",
+  authDomain: "pioneerapi.firebaseapp.com",
+  projectId: "pioneerapi",
+  storageBucket: "pioneerapi.appspot.com",
+  messagingSenderId: "180850817847",
+  appId: "1:180850817847:web:43100deedf3c711f1938b2",
+  measurementId: "G-T4VEJ3FGZ8"
 };
 
 const app = initializeApp(firebaseConfig);
