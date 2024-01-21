9e86f12d39a764128723a449635f33e3578394e6
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2024-01-21 04:05:00 +0200

    Add developer to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer footer-center p-4 text-base-content bg-indigo-100">
     <aside>
-        <p>Copyright © 2023 - All right reserved by PathPioneer</p>
+        <p>Copyright © 2023 - All right reserved by Tankiso Thebe</p>
     </aside>
 </footer>
   );
