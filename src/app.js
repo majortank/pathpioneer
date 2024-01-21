7dbebb7a0174eebd4ac261b1e9105f568a4cb163
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2024-01-21 04:06:12 +0200

    Use BrowserRouter instead of HashRouter.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './app.css';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import HomePage from './HomePage';
 import LoginPage from './LoginPage';
